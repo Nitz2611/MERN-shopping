283e7f54fa9a3dac38b4b518ad504a1b384471c5
Nitish sharma <sharmanitish183@gmail.com>
2024-02-13 18:31:28 +0530

    mongodb connection setup

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
+import morgan from 'morgan';
+import { connect } from 'mongoose';
+import connectDB from './config/db.js';
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //rest api
 app.get('/', (req, res) => {
