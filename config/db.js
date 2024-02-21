7975c86657d9a832ebd41b19bdc16998dbbf81db
Nitish sharma <sharmanitish183@gmail.com>
2024-02-21 19:34:33 +0530

    comment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import colors from "colors";
 
+// function for database connection
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL)
