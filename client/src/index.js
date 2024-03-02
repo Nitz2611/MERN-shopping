6b8f9c60528db7130bb5dc79638a8d566d88052c
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2024-03-02 12:21:34 +0530

    made use of context api and custom hook

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { AuthProvider } from './context/auth';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <Router>
+  <AuthProvider>
+    <Router>
 
-    <App />
+      <App />
 
-  </Router>
+    </Router>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -3,12 +3,14 @@ import Layout from '../../components/Layout/Layout'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../context/auth'
 
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const navigate = useNavigate()
+    const [auth, setAuth] = useAuth()
 
 
 
@@ -16,8 +18,14 @@ const Login = () => {
         e.preventDefault()
         try {
             const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, { email, password })
-            if (res.data.success) {
-                toast.success(res.data.message)
+            if (res && res.data.success) {
+                toast.success(res.data && res.data.message)
+                setAuth({
+                    ...auth,
+                    user: res.data.user,
+                    token: res.data.token,
+                })
+                localStorage.setItem('auth', JSON.stringify(res.data))
                 navigate('/');
             } else {
                 toast.error(res.data.message)
