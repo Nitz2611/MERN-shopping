366a4e68e45d29caee3b30e1103b0a99570a13cb
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2024-03-15 18:55:37 +0530

    header modified

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -55,13 +55,24 @@ const Header = () => {
                                             Login
                                         </NavLink>
                                     </li>
-                                </>) : (<>
-                                    <li className="nav-item">
-                                        <NavLink to="/login" onClick={handleLogout} className="nav-link">
-                                            Logout
-                                        </NavLink>
-                                    </li>
-                                </>)
+                                </>) : (
+                                    <>
+                                        <li className="nav-item dropdown">
+                                            <NavLink className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                {auth?.user?.name}
+                                            </NavLink>
+                                            <ul className="dropdown-menu">
+                                                <li><NavLink to="/dashboard" className="dropdown-item" >DashBoard</NavLink></li>
+                                                <li >
+                                                    <NavLink to="/login" onClick={handleLogout} className="dropdown-item">
+                                                        Logout
+                                                    </NavLink>
+                                                </li>
+                                            </ul>
+                                        </li>
+
+
+                                    </>)
                             }
                             <li className="nav-item">
                                 <NavLink to="/cart" className="nav-link">
