e4aaaf5ac860846cd2132f53a19cc5a257c3dc84
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2024-03-19 17:49:25 +0530

    private route for admin and user

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -64,7 +64,7 @@ const Header = () => {
                                                 {auth?.user?.name}
                                             </NavLink>
                                             <ul className="dropdown-menu">
-                                                <li><NavLink to="/dashboard" className="dropdown-item" >DashBoard</NavLink></li>
+                                                <li><NavLink to={`/dashboard/${auth?.user?.role === 1 ? 'admin' : "user"}`} className="dropdown-item" >DashBoard</NavLink></li>
                                                 <li >
                                                     <NavLink to="/login" onClick={handleLogout} className="dropdown-item">
                                                         Logout
--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 
-const Spinner = () => {
-    const [count, setCount] = useState(5)
+const Spinner = ({ path = 'login' }) => {
+    const [count, setCount] = useState(3)
     const navigate = useNavigate()
     const location = useLocation()
 
@@ -10,11 +10,11 @@ const Spinner = () => {
         const interval = setInterval(() => {
             setCount((prevValue) => --prevValue)
         }, 1000)
-        count === 0 && navigate('/login', {
+        count === 0 && navigate(`/${path}`, {
             state: location.pathname
         })
         return () => clearInterval(interval)
-    }, [count, navigate, location])
+    }, [count, navigate, location, path])
     return (
         <div className='d-flex flex-column justify-content-center align-items-center' style={{ height: '100vh' }}>
             <h1 className='text-center'>Please Login first.. Redirecting to you in {count} second</h1>
