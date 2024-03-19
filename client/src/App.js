53f802d49ae931e8d3bf7741448c50e160500d25
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2024-03-19 18:50:57 +0530

    admin setup

--- client/src/App.js
+++ client/src/App.js
@@ -15,23 +15,31 @@ import PrivateRoute from './components/Routes/Private'
 import ForgotPasssword from './pages/Auth/ForgotPassword'
 import AdminRoute from './components/Routes/AdminRoute'
 import AdminDashboard from './pages/Admin/AdminDashboard'
+import CreateCategory from './pages/Admin/CreateCategory'
+import CreateProduct from './pages/Admin/CreateProduct'
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Login/Register routes */}
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/forgot-password' element={<ForgotPasssword />}></Route>
-        <Route path='/' element={<HomePage />}></Route>
+
         {/* private dashboard route for user */}
         <Route path='/dashboard' element={<PrivateRoute />}>
           <Route path='user' element={<Dashboard />}></Route>
         </Route>
+
         {/* Private dashboard route for admin */}
         <Route path='/dashboard' element={<AdminRoute />}>
           <Route path='admin' element={<AdminDashboard />}></Route>
+          <Route path='admin/create-category' element={<CreateCategory />}></Route>
+          <Route path='admin/create-product' element={<CreateProduct />}></Route>
         </Route>
+
+        <Route path='/' element={<HomePage />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
         <Route path='/policy' element={<Policy />}></Route>
